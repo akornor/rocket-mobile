4eb97709ff0acccf0e4bb3143ec2621799fd0fe4
Raymond Akornor <raymondakornor@gmail.com>
2018-04-05 04:04:45 +0000

    add presale screen

---
+++ screens/PresaleScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class PresaleScreen extends Component{
+	render(){
+		return (
+			<Text>Yeah Yeah</Text>
+			)
+	}
+}
