72021fc72dd5ef5de2e0c2a149479d2e389bb5eb
Raymond Akornor <raymondakornor@gmail.com>
2018-07-16 01:18:55 +0000

    render tickets with FlatList

--- screens/TicketListScreen.js
+++ screens/TicketListScreen.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
-import {
-  Animated,
-  Dimensions,
-  View,
-  Text,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import defaultNavigationOptions from '../navigation/defaultNavOptions';
-import { Button } from 'react-native-elements';
 import CardThree from '../components/CardThree';
 
 export default class TicketListScreen extends Component {
@@ -19,25 +10,104 @@ export default class TicketListScreen extends Component {
       ...defaultNavigationOptions,
     };
   };
+
+  _keyExtractor = (item, index) => item.id;
+
+  _renderItem = ({ item }) => (
+    <CardThree
+      info={item}
+      viewMovie={() => {
+        this.props.navigation.navigate('Ticket');
+      }}
+    />
+  );
   render() {
-    const info = {
-      poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
-      original_title: 'Coco',
-      overview:
-        "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
-      release_date: '2017-10-27',
-      vote_average: 7.8,
-    };
+    const info = [
+      {
+        id: 1,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+      {
+        id: 2,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+      {
+        id: 3,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+      {
+        id: 4,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+      {
+        id: 5,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+      {
+        id: 6,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+      {
+        id: 7,
+        poster_path: '/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg',
+        original_title: 'Coco',
+        overview:
+          "Despite his family’s baffling generations-old ban on music, Miguel dreams of becoming an accomplished musician like his idol, Ernesto de la Cruz. Desperate to prove his talent, Miguel finds himself in the stunning and colorful Land of the Dead following a mysterious chain of events. Along the way, he meets charming trickster Hector, and together, they set off on an extraordinary journey to unlock the real story behind Miguel's family history.",
+        release_date: '2017-10-27',
+        vote_average: 7.8,
+      },
+    ];
     return (
-      <View style={{ padding: 10, backgroundColor: 'black', flex: 1 }}>
-        {/* <Text>TicketListScreen</Text> */}
-        <CardThree
-          info={info}
-          viewMovie={() => {
-            this.props.navigation.navigate('Ticket');
-          }}
+      <View style={styles.container}>
+        <FlatList
+          data={info}
+          renderItem={this._renderItem}
+          keyExtractor={this._keyExtractor}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: 'black',
+    flex: 1,
+  },
+  separator: {
+    marginTop: 10,
+    backgroundColor: '#8E8E8E',
+  },
+});
